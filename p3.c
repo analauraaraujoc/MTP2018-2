a1f9bb9a6a2eaefd5f5d336b15097a3aed1d60ed
analauraaraujoc <42443771+analauraaraujoc@users.noreply.github.com>
2018-10-18 09:09:21 -0300

    Add files via upload

---
+++ p3.c
@@ -0,0 +1,23 @@
+/* P3.c */
+/* Ana Laura Araújo Costa */
+/* 11811EEL020 */
+
+#include <stdio.h>
+
+int main()
+{
+    int i=0, cum=0;
+	char bits[256];  
+	scanf("%s", bits);
+for(i=0;bits[i]!= '\0';i++)
+	{
+    	if(bits[i] >= 48 && bits[i] <= 57) 
+		{
+			bits[i] = bits[i] - '0'; 
+			cum = cum * 10 + bits[i];
+		}
+	}
+	printf("\nretorna:%d", cum);
+	getchar();
+	return 0;
+}
